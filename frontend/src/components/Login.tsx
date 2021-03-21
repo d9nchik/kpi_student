5bef2ba623735a9bb028935531cf8ff08d861677
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-03-21 19:39:43 +0200

    implementation structure of Login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,7 +1,86 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import {
+  isAuthenticated,
+  loginWithEmail,
+  loginWithGithub,
+  loginWithGoogle,
+} from '../utilities/auth';
+import { Redirect, useHistory } from 'react-router-dom';
 
 const Login: FunctionComponent = () => {
-  return <div>Authentication!</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const history = useHistory();
+
+  return (
+    <div>
+      {isAuthenticated() && (
+        <Redirect to={{ pathname: '/', state: { from: '/login' } }} />
+      )}
+      <h2>Login Page</h2>
+      <h3>{message}</h3>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          if (!loginWithEmail(email, password)) {
+            setMessage('Check your email and password');
+          } else {
+            goToMainPage();
+          }
+        }}
+      >
+        <label>
+          E-mail
+          <input
+            name="email"
+            id="email"
+            type="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+      <p
+        onClick={() => {
+          if (loginWithGoogle()) {
+            goToMainPage();
+          } else {
+            setMessage('Problems with login via GOOGLE');
+          }
+        }}
+      >
+        Login with Google
+      </p>
+
+      <p
+        onClick={() => {
+          if (loginWithGithub()) {
+            goToMainPage();
+          } else {
+            setMessage('Problems with login via GitHub');
+          }
+        }}
+      >
+        Login with GitHub
+      </p>
+    </div>
+  );
+
+  function goToMainPage() {
+    history.push('/');
+  }
 };
 
 export default Login;
