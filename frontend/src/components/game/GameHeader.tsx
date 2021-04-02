67a7cc149f1da1ca4b8cdc488b3275d99364c9ed
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-02 19:22:54 +0300

    add icons to GameHeader

--- frontend/src/components/game/GameHeader.tsx
+++ frontend/src/components/game/GameHeader.tsx
@@ -1,7 +1,13 @@
 import React, { FunctionComponent } from 'react';
-
 import { GameStatus } from '../../utilities/dataStorage';
 import { calculateLevel } from '../../utilities/tools';
+
+import Money from './money.png';
+import HealthCare from './health-care.png';
+import FastFood from './fast-food.png';
+import Thought from './thought.png';
+import Shelving from './shelving.png';
+import Household from './household.png';
 
 const GameHeader: FunctionComponent<GameStatus> = ({
   satietyLevel,
@@ -17,17 +23,44 @@ const GameHeader: FunctionComponent<GameStatus> = ({
   return (
     <header>
       <p>
-        Hearts Point
+        <img
+          src={HealthCare}
+          alt="health care"
+          style={{ background: 'brown', width: '25px' }}
+        />
         <progress max={100 * level} value={heartsPoint} />
-        Satiety Level
+        <img
+          src={FastFood}
+          alt="fast food"
+          style={{ background: 'brown', width: '25px' }}
+        />
         <progress max={100 * level} value={satietyLevel} />
-        Mental Strength
+        <img
+          src={Thought}
+          alt="thought"
+          style={{ background: 'brown', width: '25px' }}
+        />
         <progress max={100 * level} value={mentalStrength} />
       </p>
       <p>
-        Money <span>{money}₴</span>
-        Education Level <span>{Math.floor(educationLevel / level)}%</span>
-        Care Level<span>{Math.floor(careLevel / level)}%</span>
+        <img
+          src={Money}
+          alt="money"
+          style={{ background: 'brown', width: '25px' }}
+        />
+        <span>{money}₴</span>
+        <img
+          src={Shelving}
+          alt="shelving"
+          style={{ background: 'brown', width: '25px' }}
+        />
+        <span>{Math.floor(educationLevel / level)}%</span>
+        <img
+          src={Household}
+          alt="household"
+          style={{ background: 'brown', width: '25px' }}
+        />
+        <span>{Math.floor(careLevel / level)}%</span>
       </p>
       *{level} <progress max={XPNeeded} value={currentXP} />
     </header>
