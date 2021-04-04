804bc2d4b6eb390df9797823922855a3ec06eaef
Nikita Sakun <NikitaSakun1@gmail.com>
2021-04-04 20:19:51 +0300

    Добавлен новый CSS файл (Register.css) в котором был прописан дизайн окна регистрации.Пофикшен баг с центрированием блока на странице авторизации(Login.css)

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState } from 'react';
 import { isAuthenticated, registerEmail } from '../../utilities/auth';
 import { Redirect, useHistory, Link } from 'react-router-dom';
-
+import './Register.css';
 const Register: FunctionComponent = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -10,7 +10,7 @@ const Register: FunctionComponent = () => {
   const history = useHistory();
 
   return (
-    <div>
+    <div id={'regWindow'}>
       <h2>Register page</h2>
       <h3>{message}</h3>
       {isAuthenticated() && (
@@ -57,7 +57,7 @@ const Register: FunctionComponent = () => {
             onChange={event => setRetypedPassword(event.target.value)}
           />
         </label>
-        <input type="submit" value="Register" />
+        <input id={'buttonReg'} type="submit" value="Register" />
       </form>
       <Link to="/login">I already have account</Link>
     </div>
