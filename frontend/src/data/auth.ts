3b776cb6fd058844d846494f6bd82fe05e07a125
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-24 20:25:01 +0300

    add executeAsyncFunctionIfErrReturnMessage to auth.ts

--- frontend/src/data/auth.ts
+++ frontend/src/data/auth.ts
@@ -9,58 +9,33 @@ const LOCALSTORAGE_AUTHORIZED_USER = 'authorized_user';
 export const loginWithEmail = async (
   email: string,
   password: string
-): Promise<string | null> => {
-  try {
-    await auth.signInWithEmailAndPassword(email, password);
-    return null;
-  } catch (error) {
-    return error.message;
-  }
-};
+): Promise<string | null> =>
+  executeAsyncFunctionIfErrReturnMessage(
+    async () => await auth.signInWithEmailAndPassword(email, password)
+  );
 
-export const loginWithGithub = async (): Promise<string | null> => {
-  try {
-    await signInWithGitHub();
-    return null;
-  } catch (error) {
-    console.error(error);
-    return error.message;
-  }
-};
+export const loginWithGithub = async (): Promise<string | null> =>
+  executeAsyncFunctionIfErrReturnMessage(signInWithGitHub);
 
-export const loginWithGoogle = async (): Promise<string | null> => {
-  try {
-    await signInWithGoogle();
-    return null;
-  } catch (error) {
-    return error.message;
-  }
-};
+export const loginWithGoogle = async (): Promise<string | null> =>
+  executeAsyncFunctionIfErrReturnMessage(signInWithGoogle);
 
 export const logout = async (): Promise<void> => await auth.signOut();
 
 export const registerEmail = async (
   email: string,
   password: string
-): Promise<string | null> => {
-  try {
-    await auth.createUserWithEmailAndPassword(email, password);
-    return null;
-  } catch (error) {
-    return error.message;
-  }
-};
+): Promise<string | null> =>
+  executeAsyncFunctionIfErrReturnMessage(
+    async () => await auth.createUserWithEmailAndPassword(email, password)
+  );
 
 export const sendPasswordResetEmail = async (
   email: string
-): Promise<string | null> => {
-  try {
-    await auth.sendPasswordResetEmail(email);
-    return null;
-  } catch (error) {
-    return error.message;
-  }
-};
+): Promise<string | null> =>
+  executeAsyncFunctionIfErrReturnMessage(
+    async () => await auth.sendPasswordResetEmail(email)
+  );
 
 let user: firebase.User | null = (() => {
   const value = localStorage.getItem(LOCALSTORAGE_AUTHORIZED_USER);
@@ -83,3 +58,14 @@ auth.onAuthStateChanged(newUser => {
 export const isAuthenticated = (): boolean => !!user;
 
 export const getUser = (): firebase.User | null => user;
+
+export async function executeAsyncFunctionIfErrReturnMessage(
+  inputFunction: () => Promise<firebase.auth.UserCredential | void>
+): Promise<string | null> {
+  try {
+    await inputFunction();
+    return null;
+  } catch (error) {
+    return error.message;
+  }
+}
