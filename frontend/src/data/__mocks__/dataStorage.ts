6f8f164a145673c233a7026648373eb49ba4d934
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-05-02 20:12:10 +0300

    add RegisterGame component test

--- frontend/src/data/__mocks__/dataStorage.ts
+++ frontend/src/data/__mocks__/dataStorage.ts
@@ -3,3 +3,38 @@ export const setGameObj = (): void => {
 };
 
 export const getDateOfRegistration = (): Date => new Date();
+
+export interface GameStatus {
+  gameLevel: number;
+  characterName: string;
+  heartsPoint: number;
+  satietyLevel: number;
+  mentalStrength: number;
+  money: number;
+  educationLevel: number;
+  careLevel: number;
+  isDead: boolean;
+}
+
+export const newGameObj: GameStatus = {
+  gameLevel: 1,
+  characterName: 'Sam',
+  heartsPoint: 100,
+  satietyLevel: 100,
+  mentalStrength: 100,
+  money: 5000,
+  educationLevel: 25,
+  careLevel: 75,
+  isDead: false,
+};
+
+export let gameObj: GameStatus = { ...newGameObj };
+
+export const newGame = (characterName: string): boolean => {
+  console.log('executed');
+
+  gameObj = { ...newGameObj, characterName };
+  return true;
+};
+
+export const getGameObj = async (): Promise<GameStatus> => gameObj;
