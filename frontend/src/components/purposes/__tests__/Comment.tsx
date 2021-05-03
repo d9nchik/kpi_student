f08a6500ac4f79649010150094abb35dedb3619b
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-05-03 18:05:20 +0300

    add test for Comment component

---
+++ frontend/src/components/purposes/__tests__/Comment.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Comment as CommentProps } from '../../../data/dataStorage';
+import Comment from '../Comment';
+
+jest.mock('../../../data/dataStorage.ts');
+jest.mock('../../../data/auth.ts');
+
+const commentProps: CommentProps = {
+  id: '123',
+  content: 'OMG!',
+  author: {
+    displayName: 'Senior developer',
+    uid: '123qwe',
+  },
+};
+
+beforeEach(() => render(<Comment {...commentProps} />));
+
+test('comment should contain author and content information', () => {
+  expect(screen.getByText('Senior developer')).toBeInTheDocument();
+  expect(screen.getByText('OMG!')).toBeInTheDocument();
+});
